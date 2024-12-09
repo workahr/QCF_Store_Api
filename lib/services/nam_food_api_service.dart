7207e0fdf5180d626a457c04c299dd96406258f8
BoseWorkahr <boseworkahr@gmail.com>
2024-12-09 11:05:33 +0530

    update screenshot api

--- lib/services/nam_food_api_service.dart
+++ lib/services/nam_food_api_service.dart
@@ -2263,4 +2263,134 @@ class NamFoodApiService {
       return e;
     }
   }
+
+  // Admin
+
+  // Store List
+  // Future getStoreList() async {
+  //   try {
+  //     final url = Uri.parse('${liveApiPath}v1/getallstorebyadmin');
+  //     final response = await client.get(
+  //       url,
+  //       headers: headerData,
+  //     );
+  //     if (response.statusCode == 200) {
+  //       return response.body;
+  //     } else {
+  //       return response;
+  //     }
+  //   } catch (e) {
+  //     return e;
+  //   }
+  // }
+
+  //Add Store
+
+  // Future addstore(
+  //     String apiCtrl, Map<String, dynamic> postData, imageFile) async {
+  //   try {
+  //     final url = Uri.parse(liveApiPath + apiCtrl);
+
+  //     var headers = headerData;
+  //     var request = http.MultipartRequest(
+  //       'POST',
+  //       url,
+  //     );
+  //     request.headers.addAll(headerData);
+
+  //     for (var entry in postData.entries) {
+  //       request.fields[entry.key] = entry.value.toString();
+  //     }
+  //     if (imageFile != null) {
+  //       var image = await http.MultipartFile.fromPath('media', imageFile!.path);
+  //       request.files.add(image);
+  //     }
+  //     request.headers.addAll(headers);
+  //     var response = await request.send();
+  //     if (response.statusCode == 200) {
+  //       final json = await response.stream.bytesToString();
+  //       return json;
+  //     } else {
+  //       return [];
+  //     }
+  //   } catch (e) {
+  //     return e;
+  //   }
+  // }
+
+// //getallorders
+//   Future getallOrderdetailslist() async {
+//     try {
+//       final url = Uri.parse('${liveApiPath}v1/getallorderbyadmin');
+//       final response = await client.get(
+//         url,
+//         headers: headerData,
+//       );
+//       if (response.statusCode == 200) {
+//         return response.body;
+//       } else {
+//         return response;
+//       }
+//     } catch (e) {
+//       return e;
+//     }
+//   }
+
+  // Future<String> getallOrderdetailslist() async {
+  //   try {
+  //     final url = Uri.parse('${liveApiPath}v1/getallorderbyadmin');
+  //     // final response = await client.get(
+  //     //   url,
+  //     //   headers: headerData,
+  //     // );
+  //     final response = await client.get(
+  //       url,
+  //       headers: headerData,
+  //     );
+  //     return response.body ?? '';
+
+  //     // Debug log for response
+  //     print('Response status: ${response.statusCode}');
+  //     print('Response body: ${response.body}');
+
+  //     if (response.statusCode == 200) {
+  //       return response.body; // Return the response body as String
+  //     } else {
+  //       // Handle non-200 status codes
+  //       print('Error: Received status code ${response.statusCode}');
+  //       return json.encode({
+  //         'status': 'ERROR',
+  //         'message':
+  //             'Failed to fetch data. Status code: ${response.statusCode}',
+  //         'data': null,
+  //       });
+  //     }
+  //   } catch (e) {
+  //     // Catch and log exceptions
+  //     print('Exception occurred: $e');
+  //     return json.encode({
+  //       'status': 'ERROR',
+  //       'message': 'An error occurred while making the API call',
+  //       'error': e.toString(),
+  //     });
+  //   }
+  // }
+
+//screenshotpage
+  Future getScreenshotpage(Id) async {
+    try {
+      final url = Uri.parse('${liveApiPath}v1/getpaymentbystore?store_id=$Id');
+      final response = await client.get(
+        url,
+        headers: headerData,
+      );
+      if (response.statusCode == 200) {
+        return response.body;
+      } else {
+        return response;
+      }
+    } catch (e) {
+      return e;
+    }
+  }
 }
